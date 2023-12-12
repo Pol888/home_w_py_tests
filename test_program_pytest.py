71d29d1ac65ec3c9ecc2c55e207ca1bff046a8f5
Павел <107019787+Pol888@users.noreply.github.com>
2023-12-12 11:22:20 +0500

    is done

--- test_program_pytest.py
+++ test_program_pytest.py
@@ -30,6 +30,19 @@ def test_comparison_of_averages_both_are_equal(capfd):
 
     assert captured.out == 'Средние значения равны\n'
 
+def test_comparison_of_averages_errors(capfd):
+    list_errors_one = [[], ['45',], [1, 2, 'ftg'], 6, '544']
+    list_errors_two = [[], ['45',], [1, 2, 'ftg'], 6, '544']
+
+    for i in list_errors_one:
+        for j in list_errors_two:
+            ListManipulation(i, j).comparison_of_averages()
+
+            captured = capfd.readouterr()
+
+            assert captured.out == 'Error\n'
+
+
 
 if __name__ == '__main__':
     pytest.main(['-v'])
